4524605c5a23ea9a799fac1732db6c3b25c591ad
Skaara9111 <skaara911@gmail.com>
2022-05-17 15:14:05 +0300

    change updatePlayerPositionProgress to rxJava

--- app/src/main/java/com/example/mediaplayer/ui/music_player/MusicPlayerFragment.kt
+++ app/src/main/java/com/example/mediaplayer/ui/music_player/MusicPlayerFragment.kt
@@ -2,6 +2,7 @@ package com.example.mediaplayer.ui.music_player
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,7 +11,6 @@ import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import com.example.mediaplayer.data.model.Result
 import com.example.mediaplayer.databinding.MusicPlayerFragmentBinding
 import com.google.android.exoplayer2.ExoPlayer
@@ -20,14 +20,15 @@ import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
 import com.google.android.exoplayer2.util.Util
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 
 class MusicPlayerFragment : Fragment() {
 
-    lateinit var viewModel : MusicPlayerViewModel
+    lateinit var viewModel: MusicPlayerViewModel
     private var seekBar: SeekBar? = null
     private var player: ExoPlayer? = null
 
@@ -39,7 +40,7 @@ class MusicPlayerFragment : Fragment() {
         val binding = MusicPlayerFragmentBinding.inflate(inflater)
         binding.lifecycleOwner = this
 
-        val selectedResult : Result = MusicPlayerFragmentArgs.fromBundle(arguments!!).resultItem
+        val selectedResult: Result = MusicPlayerFragmentArgs.fromBundle(arguments!!).resultItem
 
         val viewModelFactory = MusicPlayerViewModelFactory(selectedResult)
         viewModel = ViewModelProvider(
@@ -49,11 +50,11 @@ class MusicPlayerFragment : Fragment() {
 
         initializePlayer(selectedResult)
         viewModel.status.observe(viewLifecycleOwner, Observer {
-            when(it) {
-                PlayStatus.PLAY ->{
+            when (it) {
+                PlayStatus.PLAY -> {
                     player!!.play()
                 }
-                PlayStatus.PAUSE-> {
+                PlayStatus.PAUSE -> {
                     player!!.pause()
                 }
             }
@@ -63,7 +64,7 @@ class MusicPlayerFragment : Fragment() {
 
     private fun initializePlayer(selectedResult: Result) {
         val mediaSource: MediaSource =
-            initializeMediaSource(selectedResult)
+                initializeMediaSource(selectedResult)
 
         player = ExoPlayer.Builder(requireContext())
             .build()
@@ -86,8 +87,8 @@ class MusicPlayerFragment : Fragment() {
         player?.let { exoPlayer ->
             exoPlayer.release()
         }
+
         player = null
-        seekBar = null
     }
 
     private fun playerControls(player: ExoPlayer) {
@@ -115,27 +116,42 @@ class MusicPlayerFragment : Fragment() {
             override fun onMediaItemTransition(mediaItem: MediaItem?, reason: Int) {
                 seekBar?.progress = player.currentPosition.toInt()
                 seekBar?.max = player.duration.toInt()
-                updatePlayerPositionProgress()
+                updatePlayerPositionProgress(player.currentPosition.toInt(),
+                    player.duration.toInt())
+                    .subscribeOn(Schedulers.newThread())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+                        seekBar?.progress = player!!.currentPosition.toInt()
+                    }, {
+                    }, {
+                    })
             }
 
             override fun onPlaybackStateChanged(playbackState: Int) {
                 if (playbackState == ExoPlayer.STATE_READY) {
                     seekBar?.progress = player.currentPosition.toInt()
                     seekBar?.max = player.duration.toInt()
-                    updatePlayerPositionProgress();
+                    updatePlayerPositionProgress(player.currentPosition.toInt(),
+                        player.duration.toInt())
+                        .subscribeOn(Schedulers.newThread())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe({
+                            seekBar?.progress = player!!.currentPosition.toInt()
+                        }, {
+                        }, {
+                        })
                 }
             }
         })
     }
 
-    private fun updatePlayerPositionProgress() {
-        viewLifecycleOwner.lifecycleScope.launch {
-            coroutineScope {
+    fun updatePlayerPositionProgress(currentPosition: Int, duratation: Int): Observable<Int> {
+        return Observable.create { subscriber ->
+            for (i in 0..duratation) {
                 if (viewModel.status.value == PlayStatus.PLAY) {
-                    seekBar?.progress = player!!.currentPosition.toInt()
+                    subscriber.onNext(currentPosition)
                 }
-                delay(1000)
-                updatePlayerPositionProgress()
+                Thread.sleep(1000)
             }
         }
     }
